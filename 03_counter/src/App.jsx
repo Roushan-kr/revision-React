740499e6ec8fa328041d3de576e09645156cee40
Roushan <kumar7roushanjha2005@gmail.com>
2023-12-28 00:36:17 +0530

    gatting femilear with react hoocks

--- 03_counter/src/App.jsx
+++ 03_counter/src/App.jsx
@@ -1,35 +1,37 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect } from "react";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  // let counter = 3;
+  const [counter, setCounter] = useState(15)
 
+  // useEffect(() => {
+  // }, [counter])
+  
+  function incrCounter() {
+    if (counter==20) {
+      return
+    }
+    setCounter(counter+1);
+    console.log(counter);
+  }
+  function decrCounter() {
+    if (counter==0) {
+      return
+    }
+    setCounter(counter-1);
+    console.log(counter);
+  }
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>this is my spacce</h1>
+      <h2>Counter value is: {counter}</h2>
+
+      <button onClick={incrCounter}> {counter} +1</button>
+      <br />
+      <button onClick={decrCounter}>{counter} -1</button>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
